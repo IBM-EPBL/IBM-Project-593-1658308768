3c03dd8d094174173dd8ddd898251d9b81cd06d3
Ravi-2002 <ravisaravanan209@gmail.com>
2022-10-07 21:39:18 +0530

    change

--- Assignments/Ravikumar S - Team Member/Assignment 1/app.js
+++ Assignments/Ravikumar S - Team Member/Assignment 1/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#46dee3"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#46dee3",
         "opacity": 0.4,
         "width": 1
       },
